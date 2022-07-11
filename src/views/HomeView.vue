a8970a52d97a4a89f9c0a19ab3b6b810aaea3c18
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-11 13:06:48 +0430

    init: project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+  import HelloWorld from '../components/HelloWorld.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <HelloWorld msg="hi" />
   </main>
 </template>
