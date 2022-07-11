d15b9df537b4a9ef50bdab15b0d4b6834a4fa986
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-11 13:22:11 +0430

    feat: routers

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <script setup>
-  import { RouterView } from 'vue-router'
+
+import { RouterLink, RouterView } from 'vue-router'
+
 </script>
 
 <template>
+  <RouterLink to="/">
+    Home
+  </RouterLink>
+  <RouterLink to="/about">
+    About us
+  </RouterLink>
+  <RouterLink to="/event/1">
+    Event 1
+  </RouterLink>
+  <RouterLink to="/event/2">
+    Event 2
+  </RouterLink>
   <RouterView />
 </template>
 
 <style>
+
 @import './assets/base.css';
+
 </style>
