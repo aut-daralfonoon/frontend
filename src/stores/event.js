2d805912b5892c08c0d03089f8623fc2bfb922a3
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-12 10:32:14 +0430

    feat: events store

---
+++ src/stores/event.js
@@ -0,0 +1,43 @@
+import { defineStore } from 'pinia'
+
+export const useEventsStore = defineStore({
+  id: 'events',
+  state: () => ({
+    events: [],
+  }),
+  getters: {
+    getEvent: (state, id) => {
+      state.events.forEach((event) => {
+        if (event.id === id) {
+          return event;
+        }
+      });
+
+      return null;
+    }
+  },
+  actions: {
+    importEvents() {
+      // TODO: API fetch for list of events
+      this.events = [
+        {
+          id: 1,
+          title: "Event Title",
+          description: "Event description",
+          date: "17:00 2/2/2022",
+          poster: "https://lh3.googleusercontent.com/vtwxXwQJTzxUHeQ6xGxQuSslYd3b3W3yftMPsxcz9L1tzEmdmnPEc3cVwZpPJqGn-Yo",
+          presenter: {
+            name: "Presenter name",
+            title: "Presenter title",
+            description: "Presenter description",
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS717HKFkqCh4tbnQuw94a7zUWQmFjb3JAvepM1SVOE5WtQJyztvStgFNEOlbtaTk1d660&usqp=CAU",
+            company: {
+              name: "Company name",
+              logo: "https://cdn.logo.com/hotlink-ok/logo-social.png",
+            },
+          },
+        }
+      ]
+    },
+  }
+})
