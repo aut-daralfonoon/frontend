1c024f16f1f94174f029469a446ec9e5a70226d4
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-12 11:43:01 +0430

    edit: home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,14 +3,20 @@
 
 <template>
   <main>
-    <ul>
+    <div class="big-box">
       <!-- looping in all events and creating theirs links -->
-      <li v-for="event in eventStore.getAllEvents">
-        <RouterLink :to="'/event/' + event.id">
-          {{ "Event " + event.title + " " + event.id }}
+      <div v-for="event in eventStore.getAllEvents">
+        <h3>
+          {{ event.title }}
+        </h3>
+        <div>
+          <img :src="event.poster" :alt="event.title + '_logo'" class="beauty-img" />
+        </div>
+        <RouterLink class="view-btn" :to="'/event/' + event.id">
+          More
         </RouterLink>
-      </li>
-    </ul>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -25,3 +31,36 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.big-box {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.beauty-img {
+  width: 100px;
+  border-radius: 5px;
+}
+
+.view-btn {
+  border-radius: 5px;
+  width: 50px;
+  padding: 0 10px;
+  border: 1px solid black;
+  text-decoration: none;
+  outline: none;
+  color: black;
+  -webkit-transition-duration: 0.4s; /* Safari */
+  transition-duration: 0.4s;
+}
+
+.view-btn:hover {
+  border-radius: 10px;
+  border: 1px solid #5c5c5c;
+  color: #5c5c5c;
+  transition: all 0.8s
+}
+</style>
