f08c928e6e8282ca7f4fce487cf232f2d710183b
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-12 11:28:07 +0430

    add: base structure

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,41 @@
 <script setup>
+// importing router link and router view
 import { RouterLink, RouterView } from 'vue-router'
+// importing the event store
 import { useEventsStore } from "./stores/events";
 
-// creating our events store
-const st = useEventsStore()
-
-// importing events
-st.importEvents()
+// importing our events
+useEventsStore().importEvents()
 </script>
 
 <template>
-  <RouterLink to="/">
-    Home
-  </RouterLink>
-  <RouterLink to="/about">
-    About us
-  </RouterLink>
-  <RouterLink v-for="event in eventStore.getAllEvents" :to="'/event/' + event.id">
-    {{ "Event " + event.title + " " + event.id }}
-  </RouterLink>
-  <RouterView />
+  <main>
+    <header>
+      <RouterLink to="/">
+        Home
+      </RouterLink>
+      <RouterLink to="/about">
+        About us
+      </RouterLink>
+    </header>
+    <!-- We want to have the list of our events only in home screen -->
+    <div v-if="this.$route.name === 'home'">
+      <!-- looping in all events and creating theirs links -->
+      <ul>
+        <li v-for="event in eventStore.getAllEvents">
+          <RouterLink :to="'/event/' + event.id">
+            {{ "Event " + event.title + " " + event.id }}
+          </RouterLink>
+        </li>
+      </ul>
+    </div>
+    <!-- our router view -->
+    <RouterView />
+    <!-- footer -->
+    <footer>
+      Footer
+    </footer>
+  </main>
 </template>
 
 <script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,7 @@
 <script setup>
-
-import HelloWorld from '../components/HelloWorld.vue'
-
 </script>
 
 <template>
   <main>
-    <HelloWorld msg="home" />
   </main>
 </template>
