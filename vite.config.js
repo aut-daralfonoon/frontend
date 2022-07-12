e24d5c3cbea1c658691bb33401bb43c5d9e11e59
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-12 11:46:12 +0430

    set: proxy

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,15 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:5000',
+        ws: true,
+        changeOrigin: true,
+        crossOrigin: '*',
+      },
+    }
   }
 })
