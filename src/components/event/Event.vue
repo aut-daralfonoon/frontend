2c25b2baf5d2ccbf3315ecf8f20235d8ff755394
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-13 15:49:02 +0430

    start: event

--- src/components/event/Event.vue
+++ src/components/event/Event.vue
@@ -13,6 +13,35 @@ defineProps({
 
 <template>
   <div>
+    <div>
+      <div>
+        <img :src="event.poster" alt="poster" width="100" />
+      </div>
+      <div>
+        <div>
+          <div>
+            <img :src="event.presenter.image" alt="presenter" />
+          </div>
+          <div>
+            <img :src="event.presenter.company.logo" alt="company" />
+          </div>
+        </div>
+        <div>
+          <h3>
+            {{ event.title }}
+          </h3>
+          <p>
+            {{ event.description }}
+          </p>
+          <span>
+            Time: {{ event.time }}
+          </span>
+          <span>
+            Presenter: {{ event.presenter.name }}
+          </span>
+        </div>
+      </div>
+    </div>
     <h1 class="green">{{ event.title }}</h1>
   </div>
 </template>
