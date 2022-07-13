ba5bbe24da53109c9b3f7208b3842691d8eebfd5
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-13 11:38:11 +0430

    update: footer style

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -13,6 +13,11 @@ export default {
 <style scoped>
 /* Style the footer with a background and some padding */
 .footer {
+  /* send footer to bottom of page */
+  position:absolute;
+  bottom:0;
+  width: 100%;
+  /* other styles */
   overflow: hidden;
   font-size: 0.7em;
   background-color: #efe1ba;
