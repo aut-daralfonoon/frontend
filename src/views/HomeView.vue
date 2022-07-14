59152e9eeed8c99938ea3440b89604f821d72600
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-14 14:07:20 +0430

    update: home buttons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,11 +3,11 @@
 
 <template>
   <main>
-   <div>
-     <RouterLink to="events">
+   <div class="container">
+     <RouterLink class="c-item c-item-right" to="events">
        {{ "رویدادها" }}
      </RouterLink>
-     <RouterLink to="about">
+     <RouterLink class="c-item c-item-left" to="about">
        {{ "درباره ما" }}
      </RouterLink>
    </div>
@@ -15,4 +15,33 @@
 </template>
 
 <style scoped>
+.container {
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 90vh;
+}
+
+.c-item {
+  font-size: 1.5em;
+  padding: 10px;
+  color: #ffffff;
+  outline: none;
+  text-decoration: none;
+  width: 200px;
+  text-align: center;
+}
+
+.c-item-right {
+  background-color: #625c4e;
+  border-bottom-right-radius: 50px;
+  border-top-right-radius: 50px;
+}
+
+.c-item-left {
+  background-color: #000000;
+  border-bottom-left-radius: 50px;
+  border-top-left-radius: 50px;
+}
 </style>
