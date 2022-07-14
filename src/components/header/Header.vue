48219f20ad3e9ca4c7587202a779e10ae6e4c066
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-14 16:49:36 +0430

    add: logos in nav

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,7 +1,9 @@
 <template>
   <header class="header">
-    <RouterLink to="/" active-class="active">
-      {{ "خانه" }}
+    <RouterLink to="/" active-class="active" style="display: inline-flex; align-items: center;">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
+        <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"/>
+      </svg>
     </RouterLink>
     <RouterLink to="/events" active-class="active">
       {{ "رویدادها" }}
@@ -73,7 +75,7 @@ export default {
   text-decoration: none;
   line-height: 25px;
   border-radius: 4px;
-  margin-left: 2px;
+  margin-left: 6px;
 }
 
 /* Change the background color on mouse-over */
