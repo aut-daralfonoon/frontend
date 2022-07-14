27bccd8ed737931087a19fe5d2673f97b3b4a602
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-14 16:28:09 +0430

    add: flex menu

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,20 +1,38 @@
 <template>
-    <header class="header">
-      <RouterLink to="/" active-class="active">
-        {{ "خانه" }}
-      </RouterLink>
-      <RouterLink to="/events" active-class="active">
-        {{ "رویدادها" }}
-      </RouterLink>
-      <RouterLink to="/about" active-class="active">
-        {{ "درباره ما" }}
-      </RouterLink>
-    </header>
+  <header class="header">
+    <RouterLink to="/" active-class="active">
+      {{ "خانه" }}
+    </RouterLink>
+    <RouterLink to="/events" active-class="active">
+      {{ "رویدادها" }}
+    </RouterLink>
+    <RouterLink to="/about" active-class="active">
+      {{ "درباره ما" }}
+    </RouterLink>
+  </header>
+  <button v-on:click="toggle_display">
+    Click me
+  </button>
 </template>
 
 <script>
 export default {
-  name: "Header"
+  name: "Header",
+  mounted() {
+    this.toggle_display()
+  },
+  methods: {
+    toggle_display() {
+      const el = document.getElementsByClassName("header").item(0);
+      if (el.style.height === "55px") {
+        el.style.height = "0";
+        el.style.padding = "0"
+      } else {
+        el.style.height = "55px";
+        el.style.padding = "5px 20px";
+      }
+    }
+  }
 }
 </script>
 
@@ -24,6 +42,12 @@ export default {
   overflow: hidden;
   background-color: #efe1ba;
   padding: 5px 20px;
+  height: 55px;
+  /* setting a transition */
+  transition: all 0.5s;
+  -moz-transition: all 0.5s;
+  -webkit-transition:all 0.5s;
+  -o-transition: all 0.5s;
 }
 
 /* Style the header links */
@@ -51,8 +75,16 @@ export default {
   color: white !important;
 }
 
+.t-btn {
+  display: block;
+}
+
 /* Add media queries for responsiveness - when the screen is 500px wide or less, stack the links on top of each other */
 @media screen and (max-width: 500px) {
+  .header {
+    overflow: scroll;
+  }
+
   .header a {
     float: none;
     display: block;
