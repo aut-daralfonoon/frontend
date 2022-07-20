5f5607635beccec69bfca7ae48cf6404707f7f22
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-20 11:28:23 +0430

    add: vue scrollto

--- src/main.js
+++ src/main.js
@@ -1,11 +1,27 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueScrollTo from 'vue-scrollto'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+const vueScrollToConfig = {
+    container: 'body',
+    duration: 500,
+    easing: 'ease',
+    offset: -50,
+    force: true,
+    cancelable: true,
+    onStart: false,
+    onDone: false,
+    onCancel: false,
+    x: false,
+    y: true,
+}
+
 app.use(createPinia())
 app.use(router)
+app.use(VueScrollTo, vueScrollToConfig)
 
 app.mount('#app')
